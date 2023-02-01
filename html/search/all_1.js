ceafb539250b01bf6e34d59d8be11e8bff17997b
DamianCierlak <115574355+DamianCierlak@users.noreply.github.com>
2023-02-01 13:24:55 +0100

    documentation
    Dodanie funkcji w dokumentacji doxygen z zadania 5.

---
+++ html/search/all_1.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['calculateaverage_0',['calculateAverage',['../class_sequence.html#a308f289974a8a54da41d53436f463a0d',1,'Sequence']]],
+  ['calculatemean_1',['calculateMean',['../class_statistics.html#a9103f395622f451a9a41945898592e9a',1,'Statistics']]],
+  ['calculateproduct_2',['calculateProduct',['../class_sequence.html#a9b3ed21a7ffd32d38800ff45bc11b198',1,'Sequence']]],
+  ['calculatestandarddeviation_3',['calculateStandardDeviation',['../class_statistics.html#a74f1bbdf3217b7e5469c7742398bb749',1,'Statistics']]],
+  ['calculatesum_4',['calculateSum',['../class_sequence.html#a39e9f07adf026e1ae9cd9ea8fdb22271',1,'Sequence']]],
+  ['cierlak_5fdamian_5',['Cierlak_Damian',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ html/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['documentation_2ecpp_0',['documentation.cpp',['../documentation_8cpp.html',1,'']]]
+];
